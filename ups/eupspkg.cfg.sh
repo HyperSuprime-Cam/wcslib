98c01aeebcc86d55f6ceabbf0c5645632462d605
Mario Juric <mjuric@lsst.org>
2013-12-16 22:54:08 -0600

    Initial import of wcslib-4.14

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,4 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-cfitsioinc=$CFITSIO_DIR/include --with-cfitsiolib=$CFITSIO_DIR/lib --disable-fortran"
+NJOBS=1
