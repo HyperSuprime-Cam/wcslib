e7cc870bc77ca36d49f0cd9dc116722090aaa2af
Mario Juric <mjuric@lsst.org>
2015-05-16 15:18:04 -0700

    eupspkg.cfg.sh: search for libraries in $RLIBDIR

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,4 +1,4 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS+=" --with-cfitsioinc=$CFITSIO_DIR/include --with-cfitsiolib=$CFITSIO_DIR/lib --disable-fortran"
+CONFIGURE_OPTIONS+=" --with-cfitsioinc=$CFITSIO_DIR/include --with-cfitsiolib=$CFITSIO_DIR/$RLIBDIR --disable-fortran"
 NJOBS=1
