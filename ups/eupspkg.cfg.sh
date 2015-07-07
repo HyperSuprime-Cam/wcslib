41e463e96479e4fc07403a917f120a078a5f8770
Darko Jevremovic <darko@aob.rs>
2015-07-07 11:41:55 -0700

    Explicitly use PREFIX/lib for location of libraries
    Some systems will use lib64

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,4 +1,4 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --with-cfitsioinc=$CFITSIO_DIR/include --with-cfitsiolib=$CFITSIO_DIR/lib --disable-fortran"
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-cfitsioinc=$CFITSIO_DIR/include --with-cfitsiolib=$CFITSIO_DIR/lib --disable-fortran --libdir=$PREFIX/lib"
 NJOBS=1
